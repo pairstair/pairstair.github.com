41fdad50c31c51ea96209f7583dbf8a7b6ac647d
Mark Needham <m.h.needham@gmail.com>
2011-09-21 20:24:13 +0100

    liz/mark: deleting now unused pair stair stuff

--- pairstair.js
+++ pairstair.js
@@ -1,83 +1,3 @@
-var PairingCombination = function(cell) {
-	function lookupKey()  {
-		return cell.parent().find("td:first").text() + "-" + $(cell.parents().find("th")[cell[0].cellIndex]).text();
-	}
-	
-	function saveToCell(days) {
-		cell.text(days.join(" & "));
-	}
-	
-	function daysInCell() {
-		return _(cell.text().split(" & ")).reject(function(day) { return day == "" });
-	}
-	
-	function add(day) {
-		var days = daysInCell();
-		if(! _.include(days, day.name())) {
-			days.push(day.name());
-			saveToCell(days);
-		}
-		return obj;		
-	}
-
-	function saveInto(store) {
-		store.put(lookupKey(), daysInCell());
-	}
-	
-	function loadFrom(store) {
-		var previousDaysPaired = store.get(lookupKey()) || [];		
-		saveToCell(previousDaysPaired);
-	}
-	
-	var obj = { saveInto : saveInto, loadFrom : loadFrom, add : add };				
-	return obj;	
-}
-
-var Day = function(day) {
-	return { 
-		name : function() { 
-			return day.draggable.text(); 
-		} 
-	};
-}
-
-var DateStickies = function() {
-	var weekday=new Array(7); 
-	weekday[0]="Sunday";
-	weekday[1]="Monday";
-	weekday[2]="Tuesday";
-	weekday[3]="Wednesday";
-	weekday[4]="Thursday";
-	weekday[5]="Friday";
-	weekday[6]="Saturday";
-	date = new Date();
-	
-	function setUpDraggableDays() {		
-		_.each($( "#draggable p"), function(day){
-			if($(day).text() === weekday[date.getDay()]) {
-				$(day).addClass("today")
-			}
-		});
-
-		$( "#draggable p" ).draggable({ 
-			revert: true,
-			snap: true,
-			zIndex: 2700,
-			revertDuration: 0	
-		});
-	}
-	
-	function dayDropped(event, dayElement){ 
-		PairingCombination($(this)).add(Day(dayElement)).saveInto(store);			
-	}
-	
-	var obj = {
-		setUpDraggableDays: setUpDraggableDays, 
-		dayDropped: dayDropped
-	}
-	
-	return obj;
-}
 
 var Grid = function(rootElement) {
 	function rows() {
@@ -106,26 +26,17 @@ var Grid = function(rootElement) {
 		});		
 	}
 	
-	function toGridBuilder() {
-		var currentPeople = _.map(rootElement.find("tr th").splice(1), function(person) { return $(person).text(); });
+	function fillInPairDays(data) {
 		
-		var gridBuilder = GridBuilder();
-		$.each(currentPeople, function(index, person) {
-			gridBuilder.add(person);
-		});
-		return gridBuilder; 
 	}
-		
 		
 	var obj = {
 		rows : rows,
 		workingGrid : workingGrid,
-		init : function(eventHandlers) {
-			console.log(workingGrid());
-			
+		init : function(data) {
 			applyToGrid(inertGrid(), function (cell) { $(cell).addClass("black"); });
-		},
-		toGridBuilder : toGridBuilder
+			fillInPairDays(data);
+		}
 	};
 	return obj;
 }
@@ -144,7 +55,7 @@ var GridBuilder = function() {
 		build : function() {
 			var html = "<table id=\"droppable\">";
 			html += buildHeader();			
-			html += _.reduce(people, function(acc, person) { return acc + "<tr><td>" + person + "</td>" + addEmptyCells() + "</tr>"; }, "")
+			html += _.reduce(people, function(rows, person) { return rows + "<tr><td>" + person + "</td>" + addEmptyCells() + "</tr>"; }, "")
 			html += "</table>"
 			
 			$(".pairstair").html(html);
@@ -156,7 +67,7 @@ var GridBuilder = function() {
 	}
 	
 	function buildHeader() {
-		return  _.reduce(people, function(acc, person) { return acc + "<th>" + person + "</th>"  }, "<tr><th>Name</th>") + "</tr>";
+		return  _.reduce(people, function(columns, person) { return columns + "<th>" + person + "</th>"  }, "<tr><th>Name</th>") + "</tr>";
 	}
 	
 	return obj;
@@ -167,32 +78,15 @@ var PairStair = function () {
 		
 	var obj = {
 		init : function () {
-			$.getJSON("http://localhost:3000/git/people?jsonpCallback=?", function(data) {
-				GridBuilder().addAll(data).build();	
+			$.getJSON("http://localhost:3000/git/people?jsonpCallback=?", function(names) {
+				GridBuilder().addAll(names).build();	
 				var grid = Grid($("table"));
-				
-				resetPairStair(grid);
-				$(".reset-stair").click(function() {
-					store.reset();
-					resetPairStair(dateStickies);
-				});		
-
-				$("#add-new-person").click(function(e) {
-					grid.toGridBuilder().add($("#new-person").val()).build();
-					grid = Grid($("table"));
-					resetPairStair(grid);
-					e.preventDefault();
-				});				
-				
+				var data = [{"liz": [{"mark" : 6}, {"uday" : 88}]}];
+				grid.init(data);
 			});			
-			
 
 		}
 	};
-	
-	function resetPairStair(grid) {
-		grid.init();	
-	}
 
 	function applyToGrid(grid, fn) {
 		$.each(grid, function (idx, row) {
